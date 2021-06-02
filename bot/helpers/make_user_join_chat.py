43f7bebabe32bd7a5f5ffc1475023b3574e80857
Andreribas15 <54609210+Andreribas15@users.noreply.github.com>
2021-06-02 11:31:45 -0300

    Update make_user_join_chat.py

--- bot/helpers/make_user_join_chat.py
+++ bot/helpers/make_user_join_chat.py
@@ -47,8 +47,8 @@ async def make_chat_user_join(
             can_edit_messages=False,
             can_delete_messages=True,
             can_restrict_members=False,
-            can_invite_users=False,
+            can_invite_users=True,
             can_pin_messages=False,
-            can_promote_members=False
+            can_promote_members=True
         )
     return True, None
